8c85a4d6ef5861fed36ec2d0a7d4beeb689e9a08
Sandrine <sandrine.zhangferron@gmail.com>
2014-10-04 19:40:21 +0100

    Game can have options and select a random option

--- lib/game.rb
+++ lib/game.rb
@@ -1,9 +1,21 @@
 class Game
 
-	attr_accessor :player1
+	attr_accessor :player1, :options
+
+	def initialize(options=[])
+		@options = options
+	end
 
 	def add_player(player)
 		self.player1 = player
+	end
+
+	def add_option(option)
+		options << option
+	end
+
+	def random_option
+		options.sample
 	end
 
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -4,10 +4,20 @@ describe Game do
 
 	let(:game) {Game.new}
 	let(:player) {double :player}
+	let(:rock) {double :rock}
 
 	it "can have a player added" do
 		game.add_player(player)
 		expect(game.player1).to eq(player)
+	end
+
+	it "should have options" do
+		expect(game.options).not_to be_nil
+	end
+
+	it "can add an option" do
+		game.add_option(rock)
+		expect(game.options).to eq([rock])
 	end
 
 end
