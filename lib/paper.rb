3cd59c922c16e93def4e7110d4b03f7d3fcabb81
Sandrine <sandrine.zhangferron@gmail.com>
2014-10-05 10:39:26 +0100

    Paper has a name and can't beat scissors

--- lib/paper.rb
+++ lib/paper.rb
@@ -1,7 +1,15 @@
 class Paper
 
+	def name
+		"Paper"
+	end
+
 	def beat_rock
 		true
+	end
+
+	def beat_scissors
+		false
 	end
 
 end
--- spec/paper_spec.rb
+++ spec/paper_spec.rb
@@ -4,8 +4,16 @@ describe Paper do
 
 	let(:paper) {Paper.new}
 
+	it "should have a name" do
+		expect(paper.name).to eq("Paper")
+	end
+
 	it "should beat rock" do
 		expect(paper.beat_rock).to be true
+	end
+
+	it "shouldn't beat scissors" do
+		expect(paper.beat_scissors).to be false
 	end
 	
 end
